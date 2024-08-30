9d2a1abd4b83f35d159c40dd7f5627b5d9df57a2
Hiroshi Fujiwara <fujiwara@no1s.biz>
2024-08-30 08:29:13 +0000

    added chapter05

---
+++ chapter05/templates/App.2.jsx
@@ -0,0 +1,22 @@
+import { Work01 } from './components/Work01';
+import { Work02 } from './components/Work02';
+import { Work03 } from './components/Work03';
+import { Work04 } from './components/Work04';
+import { Work05 } from './components/Work05';
+import { Work06 } from './components/Work06';
+import { Work07 } from './components/Work07';
+import { Work08 } from './components/Work08';
+import { Work09 } from './components/Work09';
+import { Work10 } from './components/Work10';
+import { Work11 } from './components/Work11';
+import { Work12 } from './components/Work12';
+
+function App() {
+  return (
+    <div className="App">
+      <Work02 />
+    </div>
+  );
+}
+
+export default App;
